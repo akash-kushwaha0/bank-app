98e3d748becdf7d4bb8eb9c5bfa3fa28aaa0411d
akash kushwaha <akashkushwaha76986@gmail.com>
2023-04-03 14:40:11 +0530

    modern-bank-app

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
